cc710d813cb37b3f8b3e42fd65a1547e809c699f
alchemistbg <alchemist_bg@abv.bg>
2021-03-26 18:08:10 +0100

    Update 'App.styles.ts'
    Clean some tests

--- src/components/App/App.styles.ts
+++ src/components/App/App.styles.ts
@@ -2,9 +2,7 @@ import styled from 'styled-components';
 
 import { COLORS } from './../../utils/styles/stylesConstants';
 
-export const Wrapper = styled.div`
-    color: var(--color-test1);
-
+export const AppWrapper = styled.div`
     & .App{
         color: ${COLORS.textInNavigation};
     }
