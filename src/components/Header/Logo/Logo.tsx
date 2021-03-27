4313408809079eb72a729edd602caa4fedd88b01
alchemistbg <alchemist_bg@abv.bg>
2021-03-27 21:19:38 +0100

    Add initial variant on site's logo

---
+++ src/components/Header/Logo/Logo.tsx
@@ -0,0 +1,24 @@
+//Import external libraries
+
+//Import internal libraries
+import { miscConstants } from '../../../utils/misc/miscConstants';
+
+//Import components
+
+//Import styles
+
+const Logo = () => {
+    return <div className="site-logo">
+        <img
+            className="logo-image"
+            src={`${miscConstants.basicUrl}static/images/logo.png`}
+            alt="Compass Store Logo"
+        />
+        <div className="logo-text">
+            <h2 className="logo-text">Compass Shop</h2>
+            <h6 className="logo-subtext">for DEDICATED fans</h6>
+        </div>
+    </div>
+}
+
+export default Logo;
