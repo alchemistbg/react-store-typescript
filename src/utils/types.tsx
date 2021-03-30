e9db71432a15e402dce285461aeb253410609188
alchemistbg <alchemist_bg@abv.bg>
2021-03-30 17:24:01 +0200

    Add 'types.tsx'
    The file contains common types used in the project

---
+++ src/utils/types.tsx
@@ -0,0 +1,18 @@
+export type CardItemType = {
+    id: number;
+    manufacturer: string,
+    name: string,
+    image: string,
+    price: number
+}
+
+export type ProductItemType = {
+    id: number,
+    manufacturer: string,
+    name: string,
+    sizes: string[],
+    description: string,
+    images: string[],
+    price: number,
+    timesSold: number
+}
