a6c647e6c13511d00021db96426b0f1dced5b6e3
alchemistbg <alchemist_bg@abv.bg>
2021-03-31 21:11:53 +0200

    Update 'App.tsx'
    Add initial routing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,39 +2,48 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 //Import internal libraries
+import { products } from './../../utils/db.json';
 
 //Import components
 import Header from './../Header/Header';
+import Footer from './../Footer/Footer';
+import ProductList from "../ProductList/ProductList";
+import ProductItem from './../ProductItem/ProductItem';
 
 //Import styles
 import { GlobalStyle } from './../../utils/styles/GlobalStyle';
 
 import { AppWrapper } from './App.styles';
+import { miscConstants } from '../../utils/miscConstants';
 
 const App = () => {
 	return (
 		<Router>
-
-			{/* <Fragment> */}
 			<GlobalStyle />
-
-			<Header />
-
 			<AppWrapper>
-				<h3>Main</h3>
-				<Switch>
-					<Route>
 
-					</Route>
+				<Header />
 
-				</Switch>
+				<main className='site-main'>
+					<Switch>
+						<Route
+							exact
+							path={"/"}
+							// path={`${miscConstants.basicUrl}`}
+							render={() => (<ProductList productItems={products} />)}
+						/>
+						<Route
+							exact
+							// path={`${miscConstants.basicUrl}/products/:id`}
+							path={"/products/:id"}
+							component={ProductItem}
+						/>
+
+					</Switch>
+				</main>
+
+				<Footer />
 			</AppWrapper>
-
-			<footer>
-				<h5>Footer</h5>
-			</footer>
-
-			{/* </Fragment> */}
 		</Router>
 	);
 
