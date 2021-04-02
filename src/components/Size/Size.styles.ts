9951b99ef8664cb772769ca574eafd4bf4c7a859
alchemistbg <alchemist_bg@abv.bg>
2021-04-02 15:16:27 +0200

    Add component for selecting size of the shirts and corresponding styling

---
+++ src/components/Size/Size.styles.ts
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+import { COLORS } from '../../utils/styles/Constants';
+
+const SizeWrapper = styled.div`
+    padding: 1em 0;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+
+    .size-list-title{
+        flex-basis: 100%;
+        margin-top: 1em;
+    }
+
+    .label-list{
+        display: flex;
+        width: 100%;
+    }
+
+    .size-label {
+        flex-basis: 10%;
+        padding: 1em;
+        margin-right: 2%;
+        outline: 2px solid ${COLORS.outlineBorderMain};
+        text-align: center;
+
+        &:last-child {
+            margin-right: auto;
+        }
+
+        .size-radio {
+            display: none;
+        }
+
+        .size-item {
+            font-weight: bold;
+        }
+
+        &:hover {
+            color: #f0f0f0;
+            background: ${COLORS.main_Text_Default_Alpha};
+            cursor: pointer;
+        }
+
+        &.selected {
+            color: #f0f0f0;
+            background: ${COLORS.main_Text_Default}
+        }
+    }
+
+    #size-warning {
+        visibility: hidden;
+        padding: 0.5em 1em;
+        user-select: none;
+
+        &.visible {
+            visibility: visible;
+            font-weight: bold;
+            color: ${COLORS.main_Text_Reversed};
+            background: ${COLORS.main_Background_Warning};
+        }
+    }
+`;
+
+export default SizeWrapper;
---
+++ src/components/Size/Size.tsx
@@ -0,0 +1,26 @@
+
+import SizeWrapper from './Size.styles';
+
+type Props = {
+    sizes: string[];
+    handleSizeSelection(size: string): void;
+}
+
+const Size: React.FC<Props> = ({ sizes, handleSizeSelection }) => {
+    return <SizeWrapper>
+        <h6 className="size-list-title">This product is available in:</h6>
+        <div className="label-list">
+            {
+                sizes.map((size) => {
+                    return <label htmlFor="size-radio" key={size} className="size-label" id={size} onClick={() => handleSizeSelection(size)}>
+                        <input className="size-radio" name="size-radio"></input>
+                        <span className="size-item">{size}</span>
+                    </label>
+                })
+            }
+        </div>
+        <div id="size-warning">Please select a size!</div>
+    </SizeWrapper>
+}
+
+export default Size;
