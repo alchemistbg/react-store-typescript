aea7c9d9581bf8fbc3a9910cd28f5d9d2ee61655
alchemistbg <alchemist_bg@abv.bg>
2021-04-04 11:49:16 +0200

    Implement initial styling for navigation

---
+++ src/components/Header/Navigation/Navigation.styles.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+import { FONTS, COLORS } from './../../../utils/styles/Constants';
+
+const NavigationWrapper = styled.nav`
+    .nav-list{
+        list-style: none;
+        list-style-position: outside;
+        display: flex;
+    }
+
+    .nav-item{
+        line-height: 6em;
+        margin: 0;
+        padding: 0;
+    }
+
+    .cart-icon{
+        font-size: 2em;
+        position: relative;
+    }
+
+    .cart-icon:before{
+        display: block;
+        font-weight: bold;
+        font-family: "Font Awesome 5 Free";
+        content: "\f07a";
+    }
+
+    .cart-items{
+        position: absolute;
+        top: 3.5em;
+        left: 1em;
+        padding: 0.2em 0.5em 0.4em 0.5em;
+        border-radius: 50%;
+        line-height: 1;
+        font-size: 0.5em;
+        font-family: ${FONTS.fontFamilyHeaders};
+        font-weight: bold;
+        background-color: ${COLORS.main_Background_Warning};
+    }
+`;
+
+export default NavigationWrapper;
