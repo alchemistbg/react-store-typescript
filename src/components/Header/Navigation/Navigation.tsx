363b330e60cf02fe429f058219c330fa0ce7e4f7
alchemistbg <alchemist_bg@abv.bg>
2021-04-04 11:48:48 +0200

    Implement initial version of the navigation

--- src/components/Header/Navigation/Navigation.tsx
+++ src/components/Header/Navigation/Navigation.tsx
@@ -1,1 +1,41 @@
-export { };
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import NavigationWrapper from './Navigation.styles';
+import CartContext from './../../../contexts/CartContext';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+const Navigation = () => {
+    const [cartItemsCount, setCartItemsCount] = useState<number>(0);
+
+    const cartContext = useContext(CartContext);
+
+    useEffect(() => {
+        let _count = 0;
+
+        for (let key in cartContext.cartState.items) {
+            _count += cartContext.cartState.items[key].length;
+        }
+
+        if (_count === cartItemsCount) {
+            return
+        };
+
+        setCartItemsCount(_count);
+    }, [cartContext, cartItemsCount]);
+
+    return <NavigationWrapper>
+        <ul className="nav-list">
+            <li className="nav-item cart">
+                {/* <Link to={`${miscConstants.basicUrl}/cart`}> */}
+                <Link to={`/cart`}>
+                    <span className="cart-icon">
+                        <p className="cart-items">{cartItemsCount}</p>
+                    </span>
+                </Link>
+            </li>
+        </ul>
+    </NavigationWrapper>
+}
+
+export default Navigation;
