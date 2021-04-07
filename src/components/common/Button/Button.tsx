269eadd6496f4ca3a4b9ae79134e22fc4606dc51
alchemistbg <alchemist_bg@abv.bg>
2021-04-07 21:07:21 +0200

    Add possibility to pass onClick handler to button component

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -4,18 +4,18 @@ type Props = {
     dataTestId: string;
     isDisabled: boolean;
     className: string;
-    // onClick
+    onClick: React.MouseEventHandler<HTMLButtonElement>;
     btnText: string;
     icon: string;
 }
 
-const Button: React.FC<Props> = ({ dataTestId = "", isDisabled, className, btnText, icon = "" }) => {
+const Button: React.FC<Props> = ({ dataTestId = "", isDisabled, className, onClick, btnText, icon = "" }) => {
     return <ButtonWrapper>
         <button
             data-testid={dataTestId}
             disabled={isDisabled}
             className={className}
-        //function
+            onClick={onClick}
         >
             {icon}{btnText}
         </button>
