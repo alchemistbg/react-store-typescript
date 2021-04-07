7d5d8971f6814dce89b8f44ee7471524b466f33c
alchemistbg <alchemist_bg@abv.bg>
2021-04-07 21:11:35 +0200

    Fix some small issues
    Missing bracket
    
    Wrond indentation

--- src/components/ProductList/ProductList.styles.ts
+++ src/components/ProductList/ProductList.styles.ts
@@ -2,18 +2,19 @@ import styled from 'styled-components';
 
 export const ProductListWrapper = styled.div`
     .product-list {
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
 
-    &::after {
-        content: "";
-        width: 32%;
-    }
+        &::after {
+            content: "";
+            width: 32%;
+        }
 
-    .list-title {
-        width: 100%;
-        font-weight: bold;
-        margin-bottom: 0.5em;
+        .list-title {
+            width: 100%;
+            font-weight: bold;
+            margin-bottom: 0.5em;
+        }   
     }
 `;
