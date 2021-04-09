b2b908c8a30e3a47195c850448f2e4b80218d40e
alchemistbg <alchemist_bg@abv.bg>
2021-04-09 21:30:43 +0200

    Update Header component
    Add CartContext import
    
    Add handler that gets cart content from user's localStorage
    
    Add Navigation component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,6 @@
+import { useContext } from 'react';
+import CartContext from './../../contexts/CartContext';
+
 //Import external libraries
 import { Link } from 'react-router-dom';
 
@@ -6,18 +9,33 @@ import { miscConstants } from '../../utils/miscConstants';
 
 //Import components
 import Logo from './../common/Logo/Logo';
+import Navigation from './Navigation/Navigation';
 
 //Import styles
-import { HeaderWrapper } from './Header.styles';
+import HeaderWrapper from './Header.styles';
 
 const Header = () => {
+    const cartContext = useContext(CartContext);
+
+    const handleOnLoad = () => {
+        const storedCart = localStorage.getItem("cartData");
+        if (storedCart !== null) {
+            cartContext.cartDispatch({
+                type: "LOAD_CART_FROM_STORAGE",
+                payload: JSON.parse(storedCart),
+                cartContent: JSON.parse(storedCart),
+            });
+        }
+    }
+
     return (
         <HeaderWrapper>
-            <div className='site-header'>
+            <div className='site-header' onLoad={handleOnLoad}>
                 {/* <Link to={`${miscConstants.basicUrl}`}> */}
                 <Link to="/">
                     <Logo />
                 </Link>
+                <Navigation />
             </div>
         </HeaderWrapper>
     );
