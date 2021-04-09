e1d7f50b5959eda397c22dd51bbfe1dec3533050
alchemistbg <alchemist_bg@abv.bg>
2021-04-09 21:39:49 +0200

    Add Quantity component
    It is reponsible for changing numbers of items in the Cart component
    
    The component is in working condition
    
    Styling needs refinement

--- src/components/common/Quantity/Quantity.styles.ts
+++ src/components/common/Quantity/Quantity.styles.ts
@@ -1,1 +1,16 @@
-export { };
+import styled from 'styled-components';
+
+import { COLORS } from './../../../utils/styles/Constants';
+
+const QuantityWrapper = styled.span.attrs((props) => ({
+    className: props.className
+}))`
+    display: flex;
+    justify-content: space-around;
+    align-items: baseline;
+    /* margin: 0 auto; */
+
+    
+`;
+
+export default QuantityWrapper;
--- src/components/common/Quantity/Quantity.tsx
+++ src/components/common/Quantity/Quantity.tsx
@@ -1,5 +1,46 @@
-const Quantity = () => {
-    return <div>Quantity</div>
+import Button from './../Button/Button';
+import IProductItem from './../../../utils/interfaces/IProductItem';
+import ProductItem from './../../ProductItem/ProductItem';
+import IQuantityProps from './../../../utils/interfaces/Props/IQuantityProps';
+import QuantityWrapper from './Quantity.styles';
+
+const Quantity: React.FC<IQuantityProps> = ({ className, productItem, productQty, handleDecrement, handleIncrement }) => {
+    let qty;
+    if (productItem) {
+        qty = productItem.productQty;
+    } else {
+        qty = productQty;
+    }
+
+    return <QuantityWrapper className={className}>
+        {
+            qty === 1 ? (
+                <Button
+                    dataTestId={"decr"}
+                    isDisabled={true}
+                    className="form-button decrement"
+                    onClick={handleDecrement}
+                    btnText={""}
+                    icon="" />
+            ) : (
+                <Button
+                    dataTestId={"decr"}
+                    isDisabled={false}
+                    className="form-button decrement"
+                    onClick={handleDecrement}
+                    btnText={""}
+                    icon="" />
+            )
+        }
+        {qty}
+        <Button
+            dataTestId={"incr"}
+            isDisabled={false}
+            className="form-button increment"
+            onClick={handleIncrement}
+            btnText={""}
+            icon="" />
+    </QuantityWrapper>;
 }
 
 export default Quantity;
