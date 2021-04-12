1a376c64bd81eb4b1153d7d8dcbafd3d47b35e84
alchemistbg <alchemist_bg@abv.bg>
2021-04-12 20:37:33 +0200

    Add 'Sort by' menu in 'ProductList'
    It is based on the 'Select' component
    
    It is still in testing phase

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,19 +1,50 @@
+import { useState } from 'react';
 import IProductItem from '../../utils/interfaces/IProductItem';
 import ProductCard from '../ProductCard/ProductCard';
 // import { ProductItemType } from '../../utils/types/types';
 
 
 import { ProductListWrapper } from './ProductList.styles';
+import Select from './../common/Select/Select';
 
 type Props = {
     productItems: IProductItem[]
 }
 
 const ProductList: React.FC<Props> = ({ productItems }) => {
+    const selectListItems = ['123', '1', '2'];
+
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState(selectListItems[0]);
+
+    const onClickHandler = (evt: React.SyntheticEvent<HTMLLIElement>) => {
+        const clickedItem = evt.currentTarget as HTMLLIElement;
+        setSelectedOption(clickedItem.innerText);
+        setIsOpen(false);
+    };
+
+    const toggleOpen = () => {
+        setIsOpen(!isOpen);
+    }
+
     return (
         <ProductListWrapper>
             <h1 className="list-title">Our Products</h1>
 
+            <span className="sort-select-container">
+                <h6 className={`sort-select-header ${isOpen ? "opened" : ""}`} onClick={toggleOpen}>Sort by: {selectedOption}</h6>
+                {
+                    isOpen &&
+                    <Select
+                        // className=""
+                        selectListClassName="sort-select-list"
+                        selectItemClassName="sort-select-item"
+                        selectListItems={selectListItems}
+                        onClickHandler={onClickHandler}
+                        selectedOption={selectedOption}
+                    />
+                }
+            </span>
             <div className="product-list">
                 {
                     productItems.map((product => {
