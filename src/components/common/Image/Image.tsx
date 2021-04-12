3272bb067037b5ece3e7d67e6e279b619299fb75
alchemistbg <alchemist_bg@abv.bg>
2021-04-12 20:26:32 +0200

    Update 'Image' component
    Some code was commented as it is unnecessary for now
    
    The code needs to be cleaned

--- src/components/common/Image/Image.tsx
+++ src/components/common/Image/Image.tsx
@@ -1,14 +1,16 @@
 import { SyntheticEvent } from 'react';
-import { ProductItemType } from '../../../utils/types';
+// import { ProductItemType } from '../../../utils/types/types';
+// import IProductItem from './../../../utils/interfaces/IProductItem';
+import IImageProps from './../../../utils/interfaces/Props/IImageProps';
 import { miscConstants } from './../../../utils/miscConstants';
 
-type Props = {
-    image: string;
-    imageClass: string;
-    productItemName: string;
-}
+// type Props = {
+//     image: string;
+//     imageClass: string;
+//     productItemName: string;
+// }
 
-const Image: React.FC<Props> = ({ image, imageClass, productItemName }) => {
+const Image: React.FC<IImageProps> = ({ image, imageClass, productItemName }) => {
 
     const handleImageLoadError = (evt: SyntheticEvent<HTMLImageElement, Event>) => {
         // evt.currentTarget.src = `${miscConstants.basicUrl}/static/images/error.png`;
