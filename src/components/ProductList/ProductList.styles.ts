aa05dfc82bb9823fcb8b08ed8a82add851fdbb17
alchemistbg <alchemist_bg@abv.bg>
2021-04-12 20:36:05 +0200

    Add styling for 'Sort by' menu

--- src/components/ProductList/ProductList.styles.ts
+++ src/components/ProductList/ProductList.styles.ts
@@ -1,7 +1,79 @@
 import styled from 'styled-components';
+import { CONTENT_WIDTHS, COLORS, SHADOWS } from './../../utils/styles/Constants';
 
 export const ProductListWrapper = styled.div`
+    position: relative;
+
+    .sort-select-container{
+        background-color: ${COLORS.main_Background_Main};
+        width: ${CONTENT_WIDTHS.componentWidth_Default};
+        position: absolute;
+        z-index: 200;
+        cursor: pointer;
+
+        .sort-select-header, .sort-select-list{
+            box-shadow: ${SHADOWS.shadowShapeMain} ${SHADOWS.shadowColorLight};
+        }
+
+        .sort-select-header{
+            margin: 0;
+            padding: 0.5em 0.5em;
+
+            &:after{
+                font-family: 'Font Awesome 5 Free';
+                content: '\f078';
+                font-weight: bold;
+                position: absolute;
+                right: 0.5em;
+            }
+
+            &.opened:after{
+                content: '\f077';
+            }
+        }
+
+        .sort-select-list{
+            list-style: none;
+            list-style-position: outside;
+            border-top: 1px solid ${COLORS.borderMain};
+        }
+
+        .sort-select-item{
+            padding: 0.5em 0.5em;
+            
+            &:before{
+                font-family: 'Font Awesome 5 Free';
+                content: '\f111';
+                padding-right: 0.5em;
+            }
+        }
+
+        .sort-select-item:hover{
+            color: ${COLORS.main_Text_Highlighted};
+            background-color: ${COLORS.main_Background_Reversed_Alpha50};
+        }
+
+        .sort-select-item:hover:before{
+            content: '\f192';
+        }
+
+        .sort-select-item.selected{
+            color: ${COLORS.main_Text_Highlighted};
+            background-color: ${COLORS.main_Background_Reversed_Alpha25};
+            
+            &:before{
+                content: '\f192';
+            }
+
+            &:hover{
+                background-color: ${COLORS.main_Background_Reversed_Alpha50};
+            }
+        }
+    }
+
     .product-list {
+        flex-basis: 100%;
+        margin-top: 6em;
         display: flex;
         justify-content: space-between;
         flex-wrap: wrap;
