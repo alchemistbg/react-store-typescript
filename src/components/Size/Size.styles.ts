4e5b2b19311761c63749905a50542d86a60fc083
alchemistbg <alchemist_bg@abv.bg>
2021-04-22 20:28:28 +0200

    Minor update of the styles for 'Size' component
    Reflects new names for some of the classes
    
    Add some media queries

--- src/components/Size/Size.styles.ts
+++ src/components/Size/Size.styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { COLORS } from '../../utils/styles/Constants';
+import { CONTENT_WIDTHS, DEVICE_MAX_WIDTH, COLORS } from '../../utils/styles/Constants';
 
 const SizeWrapper = styled.div`
     padding: 1em 0;
@@ -12,14 +12,31 @@ const SizeWrapper = styled.div`
         margin-top: 1em;
     }
 
-    .label-list{
+    .size-label-list{
         display: flex;
+        /* justify-content: space-between; */
+        /* padding: 0; */
+        /* margin: 0; */
         width: 100%;
+        /* width: 50%; */
+    }
+
+    @media ${DEVICE_MAX_WIDTH.laptopM}{
+        .size-label-list{
+            width: 90%;
+        }
+    }
+
+    @media ${DEVICE_MAX_WIDTH.tablet}{
+        .size-label-list{
+            width: 100%;
+        }
     }
 
     .size-label {
         flex-basis: 10%;
-        padding: 1em;
+        /* width: 15%; */
+        padding: 0.8em;
         margin-right: 2%;
         outline: 2px solid ${COLORS.outlineBorderMain};
         text-align: center;
