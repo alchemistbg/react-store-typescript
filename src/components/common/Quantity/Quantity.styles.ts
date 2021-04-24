fed55b1c284b013044aaf2ccbc81b07ab939c3a1
alchemistbg <alchemist_bg@abv.bg>
2021-04-24 14:38:22 +0200

    Update styles for 'Quantity' component
    Add simple styles for the number of products in cart for example

--- src/components/common/Quantity/Quantity.styles.ts
+++ src/components/common/Quantity/Quantity.styles.ts
@@ -1,16 +1,21 @@
 import styled from 'styled-components';
 
-import { COLORS } from './../../../utils/styles/Constants';
+// import { COLORS } from './../../../utils/styles/Constants';
 
 const QuantityWrapper = styled.span.attrs((props) => ({
     className: props.className
 }))`
     display: flex;
     justify-content: space-around;
-    align-items: baseline;
-    /* margin: 0 auto; */
+    align-items: center;
 
-    
+    .qty-number {
+        /* width: 1.5em; */
+        font-size: 1.5em;
+        font-weight: bold;
+        text-align: center;
+        user-select: none;
+    }
 `;
 
 export default QuantityWrapper;
