8436582cc4e21ce6a0776e3d0a4b23682d0f57cf
alchemistbg <alchemist_bg@abv.bg>
2021-04-24 14:33:43 +0200

    Update styles for the 'Cart' component
    Add simple rules for styling total price and checkout button

--- src/components/Cart/Cart.styles.ts
+++ src/components/Cart/Cart.styles.ts
@@ -1,7 +1,24 @@
 import styled from 'styled-components';
 
 const CartWrapper = styled.div`
-    
+    .total-price-wrapper{
+
+        width: 40%;
+        margin-left: auto;
+        
+        h4{
+            display: flex;
+            justify-content: space-between;
+        }
+
+        .text {
+            font-weight: normal;
+        }
+
+    }
+
+
+
 `;
 
 export default CartWrapper;
