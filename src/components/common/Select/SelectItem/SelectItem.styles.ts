9f3df3b43fb44d28a76c8cd273de9f5b8132bf9f
alchemistbg <alchemist_bg@abv.bg>
2021-04-26 18:02:32 +0200

    Add style for the 'SelectItem' component
    They are empty at the moment
    
    For the styling is responsible 'ProductList' component

--- src/components/common/Select/SelectItem/SelectItem.styles.ts
+++ src/components/common/Select/SelectItem/SelectItem.styles.ts
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { COLORS } from './../../../../utils/styles/Constants';
 
 const SelectItemWrapper = styled.li.attrs((props) => ({
     className: props.className
