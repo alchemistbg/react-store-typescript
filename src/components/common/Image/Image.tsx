e81b6f23b8bc1627a0a41ea04ca06808a32f42eb
alchemistbg <alchemist_bg@abv.bg>
2021-04-28 19:27:15 +0200

    Update 'Image' component
    Remove unused imports
    
    Remove some commented code

--- src/components/common/Image/Image.tsx
+++ src/components/common/Image/Image.tsx
@@ -1,26 +1,18 @@
 import { SyntheticEvent } from 'react';
-// import { ProductItemType } from '../../../utils/types/types';
-// import IProductItem from './../../../utils/interfaces/IProductItem';
 import IImageProps from './../../../utils/interfaces/Props/IImageProps';
 import { miscConstants } from './../../../utils/miscConstants';
-
-// type Props = {
-//     image: string;
-//     imageClass: string;
-//     productItemName: string;
-// }
 
 const Image: React.FC<IImageProps> = ({ image, imageClass, productItemName }) => {
 
     const handleImageLoadError = (evt: SyntheticEvent<HTMLImageElement, Event>) => {
-        // evt.currentTarget.src = `${miscConstants.basicUrl}/static/images/error.png`;
-        evt.currentTarget.src = `/static/images/error.png`;
+        // evt.currentTarget.src = `/static/images/error.png`;
+        evt.currentTarget.src = `${miscConstants.baseUrl}/static/images/error.png`;
     }
 
     return <span className={imageClass}>
         <img
-            // src={`${miscConstants.basicUrl}/static/images/` + image}
-            src={`/static/images/` + image}
+            // src={`/static/images/` + image}
+            src={`${miscConstants.baseUrl}/static/images/` + image}
             alt={`${productItemName}`}
             onError={handleImageLoadError}
 
